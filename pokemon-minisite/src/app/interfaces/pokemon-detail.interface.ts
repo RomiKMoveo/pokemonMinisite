662bac2c55ab219465697b15573f3a151cf1776b
romi kamin <romik@moveo.co.il>
2024-04-20 14:25:12 +0300

    creating pokemon interface for the front page, and an additional interface pokemon details, for the pop-up page about each pokemon

---
+++ pokemon-minisite/src/app/interfaces/pokemon-detail.interface.ts
@@ -0,0 +1,32 @@
+export interface Resource {
+    name: string;
+    url: string;
+}
+
+export interface RawAbility {
+    ability: Resource;
+    slot: number;
+    is_hidden: boolean;
+}
+
+export interface RawStats {
+    base_stat: number;
+    effort: number;
+    stat: Resource;
+}
+   
+
+export interface PokemonInterface {
+    id: number;
+    name: string;
+    imageUrl: string;
+    types: string[]; 
+    height: number;
+    weight: number;
+    stats: RawStats [],
+    abilities: RawAbility[]
+}
+
+    
+
+
