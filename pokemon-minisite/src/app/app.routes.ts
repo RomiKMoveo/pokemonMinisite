253c43a70e963ad2d2b209ab0bc01849c6bc4ae6
romi kamin <romik@moveo.co.il>
2024-04-25 13:29:25 +0300

    history component  - done without style

--- pokemon-minisite/src/app/app.routes.ts
+++ pokemon-minisite/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import { HistoryComponent } from './components/history/history.component';
 import { LoginComponent } from './components/login/login.component';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginService } from './services/login.service';
 
 export const routes: Routes = [
     { path: '', redirectTo:'login', pathMatch:'full'},
-    { path: 'pokemons', component: PokemonListComponent},
-    { path: 'history', component: HistoryComponent},
+    { path: 'pokemons', component: PokemonListComponent },
+    { path: 'history', component: HistoryComponent },
     { path: 'login', component: LoginComponent},
     { path: '**', redirectTo:'login', pathMatch:'full' }
   ];
--- pokemon-minisite/src/app/components/history/history.component.ts
+++ pokemon-minisite/src/app/components/history/history.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    FormsModule
+  ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit{
+  searchHistory: string[] = [];
+
+  constructor(
+    private pokemonListComponent: PokemonListComponent,
+    private dialog: MatDialog,
+    private router: Router,
+    private loginService: LoginService,
+    ) {
+  
+     
+    
+    }
+  
+  ngOnInit(): void {
+    
+    if (this.loginService.getisVerifiedSubject() === false) {
+      this.router.navigate(['/login']);
+    }
+    const searchHistoryLS = localStorage.getItem('searchHistory');
+    this.searchHistory = searchHistoryLS ? JSON.parse(searchHistoryLS) : [];
+    console.log(searchHistoryLS);
+    console.log(this.searchHistory);
+   
+  }
 
 }
