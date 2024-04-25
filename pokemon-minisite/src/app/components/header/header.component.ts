2f34ab141db2f54e23dd67abc22974464e13b3fb
romi kamin <romik@moveo.co.il>
2024-04-25 09:15:51 +0300

    moved header to app component so it will appear in every page

--- pokemon-minisite/src/app/components/header/header.component.ts
+++ pokemon-minisite/src/app/components/header/header.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+//import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isLoggedIn: boolean = false;
+  
+  constructor(
+    private router: Router
+  ) {}
+  
+  ngOnInit(): void {
+    this.isLoggedIn = true;
+  }
 
+  logout() {
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+
+  showHistory() {
+    this.router.navigate(['/history']);
+
+  }
   
 }
