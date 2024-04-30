0685e9f985e5e8650da073fd2cfec1e0050f6139
romi kamin <romik@moveo.co.il>
2024-04-30 18:01:56 +0300

    created a new myMap component

---
+++ pokemon-minisite/src/app/components/my-map/my-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-map.component.html',
+  styleUrl: './my-map.component.scss'
+})
+export class MyMapComponent {
+
+}
