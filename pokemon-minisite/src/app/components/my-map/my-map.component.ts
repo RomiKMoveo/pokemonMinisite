eb2e64cf9826d3e868097d9ed30b37b043753513
romi kamin <romik@moveo.co.il>
2024-05-02 14:04:44 +0300

    moved initialization to a designated function

--- pokemon-minisite/src/app/components/my-map/my-map.component.ts
+++ pokemon-minisite/src/app/components/my-map/my-map.component.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { myMapStyle } from './constants/my-map-style';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { Coordinates } from '../../interfaces/addres-search-result.interface';
 
 @Component({
 selector: 'app-my-map',
@@ -20,41 +21,29 @@ templateUrl: './my-map.component.html',
 styleUrl: './my-map.component.scss'
 })
 export class MyMapComponent implements AfterViewInit{
+  readonly homeCoordinates: Coordinates = {
+    lat: 32.055010,
+    lng: 34.856850
+  };
+  readonly officeCoordinates: Coordinates = {
+    lat: 32.0624076,
+    lng: 34.7707653
+  };
+  
   @ViewChild('mapContainer', { static: false }) 
-  gmap!: ElementRef;
+  googleMap!: ElementRef;
 
   map!: google.maps.Map;
-  lat = 32.0624076;
-  lng = 34.7707653;
-
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
-
-  mapOptions: google.maps.MapOptions = {
-    center: this.coordinates,
-    zoom: 15
-  };
-
-  marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
-    title: "Office"
-  });
+  marker!: google.maps.Marker;
   
   @Input()  placeholder=""; 
   @ViewChild('searchInput')
   searchInput!: ElementRef;
   autocomplete: google.maps.places.Autocomplete | undefined;
 
-  private directionsService = new google.maps.DirectionsService();
   private directionsRenderer = new google.maps.DirectionsRenderer();
-  readonly homeCoordinates = {
-    lat: 32.055010,
-    lng: 34.856850
-  };
-  readonly officeCoordinates = {
-    lat: 32.0624076,
-    lng: 34.7707653
-  };
+  
+  
 
   isStyleChanged: boolean = false;
 
@@ -64,7 +53,18 @@ export class MyMapComponent implements AfterViewInit{
   }
 
   mapInitializer(): void {
-    this.map = new google.maps.Map(this.gmap?.nativeElement, this.mapOptions);
+    const mapOptions: google.maps.MapOptions = {
+      center: this.officeCoordinates,
+      zoom: 15
+    };
+
+    this.marker = new google.maps.Marker({
+      position: this.officeCoordinates,
+      map: this.map,
+      title: "Office"
+    });
+
+    this.map = new google.maps.Map(this.googleMap?.nativeElement, mapOptions);
     this.marker.setMap(this.map);
   }
 
@@ -89,13 +89,15 @@ export class MyMapComponent implements AfterViewInit{
   }
 
   getDirections(): void {
+    const directionsService = new google.maps.DirectionsService();
+    
     const request = {
       origin: this.homeCoordinates,
       destination: this.officeCoordinates,
       travelMode: google.maps.TravelMode.DRIVING,
     };
     this.directionsRenderer.setMap(this.map);
-    this.directionsService.route(request, (result, status) => {
+    directionsService.route(request, (result, status) => {
       if (status === 'OK') {
         this.directionsRenderer.setDirections(result);
       }
--- pokemon-minisite/src/app/interfaces/addres-search-result.interface.ts
+++ pokemon-minisite/src/app/interfaces/addres-search-result.interface.ts
@@ -1,7 +1,5 @@
-export interface AddresSearchResult {
-    address: string;
-    location?: google.maps.LatLng;
-    iconUrl?: string;
-    name?: string; 
+export interface Coordinates {
+  lat: number;
+  lng: number;
   }
   
